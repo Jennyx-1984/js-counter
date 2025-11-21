5fa0d0dd6a914f47be5860fea3531e9c7fdeaa2e
Jennyx-1984 <163904901+Jennyx-1984@users.noreply.github.com>
2025-11-21 20:07:01 +0100

    finalizado contador

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,25 @@
+function getNumber() {
+    return document.getElementById("number");
+}
+
+
 function add(){
     //Escribir tu código que aumenta
+    const number = getNumber();
+    number.innerHTML = String(parseInt(number.innerHTML) + 1);
 }
 
 function reset(){
     //Escribir tu código que hace un reset a 0
+    const number = getNumber();
+    number.innerHTML = 0;
 }
 
 function decrease(){
     //Escribir tu código que resta
+    const number = getNumber();
+    let current = String(parseInt(number.innerHTML));
+    number.innerHTML = current > 0 ? current - 1 : 0;
 };
 
 export {add, reset, decrease}
--- src/js/events.js
+++ src/js/events.js
@@ -2,3 +2,9 @@ import {add, reset, decrease} from './counter.js'
 
 
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
+let addButton = document.getElementById("add");
+let resetButton = document.getElementById("reset");
+let decreaseButton = document.getElementById("decrease");
+addButton.addEventListener("click", add);
+resetButton.addEventListener("click", reset);
+decreaseButton.addEventListener("click", decrease);
